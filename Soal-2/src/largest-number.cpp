2e8990052ba646c1cba3970129f21de8c515ab8a
Muhhae <haekal.muhamat@gmail.com>
2023-05-23 19:16:22 +0700

    Finally fix

--- Soal-2/src/largest-number.cpp
+++ Soal-2/src/largest-number.cpp
@@ -1,6 +1,7 @@
 #include "include/largest-number.hpp"
 #include <iostream>
 #include <conio.h>
+#include <vector>
 
 LargestNumber::LargestNumber(){
 
@@ -8,53 +9,50 @@ LargestNumber::LargestNumber(){
 
 bool LargestNumber::compare(int _a, int _b)
 {
+    // std::cout<<"A = "<<_a<<" B = "<<_b<<std::endl;
+
     if (_a == _b)
     {
         return false;
     }
 
-    int aC = 1, bC = 1;
-    float a = (float)_a, b = (float)_b;
+    std::vector<int> A_digit;
+    std::vector<int> B_digit;
 
-    while (a >= 10) 
+    if (_a == 0) A_digit.push_back(0);
+    if (_b == 0) B_digit.push_back(0);
+
+    while (_a > 0) 
     {
-        aC *= 10; 
-        a /= 10;
+        A_digit.push_back(_a % 10);
+        _a /= 10;
     }
 
-    while (b >= 10) 
+    while (_b > 0) 
     {
-        bC *= 10;
-        b /= 10;
+        B_digit.push_back(_b % 10);
+        _b /= 10;
     }
 
-    int mod = aC > bC ? aC : bC;
+    int iA = A_digit.size() - 1, iB = B_digit.size() - 1;
 
-    // std::cout<<a<<" "<<b<<std::endl;
-    
-    while ((int)a == (int)b && mod != 1)
+    while (1)
     {
-        //std::cout<<"tes"<<std::endl;
-
-        // std::cout<<"_a = "<<_a<<" _a % mod = "<<(_a % mod)<<"_b = "<<_b<<" _b % mod = "<<(_b % mod)<<std::endl;
+        // std::cout<<"A = "<<A_digit[iA]<<" B = "<<B_digit[iB]<<std::endl;
         // getch();
 
-        if ( ( (_a % mod) != (_b % mod) ) /*&& (_a % mod != 0) && (_b % mod != 0)*/ )
+        if (A_digit[iA] == B_digit[iB])
         {
-            return (_a % mod) > (_b % mod);
+            if (iA > 0) iA--;
+            if (iB > 0) iB--;
         }
 
-        // 10 1
+        else return A_digit[iA] > B_digit[iB];
 
-        // std::cout<<"mod = "<<mod<<" "<<(_a % mod)<<" "<<" "<<(_b % mod)<<std::endl;
-        // getch();
-
-        mod /= 10;
-    } 
-
-    //if (a == b) return _a < _b;
-
-    return a > b;
+        if (A_digit[iA] == B_digit[iB] && iA <= 0 && iB <= 0) return false;
+    }
+    
+    return 0;
 }
 
 void LargestNumber::sortNumbers()
